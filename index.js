c497652fd2436f89fcc9d48f9b41e3941e52aca8
Bilal <bilaljameel2323@gmail.com>
2023-09-25 21:53:07 +0530

    new update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-document.getElementById("count-el").innerText = 9
+// document.getElementById("count-el").innerText = 9
+
+// console.log(count)
+// let myAge = 20 + 2
+// console.log(myAge)
+let newELE = document.getElementById("count-el")
+let count = 0
+function increament(){
+    count = count + 1
+    newELE.innerText = count
+
+}
+
+function saveDoc(){
+    let saveELE =document.getElementById("saved")
+    let countStr = count+" - "
+    saveELE.textContent = saveELE.textContent + countStr
+
+}
